66987af7b6dc1c3dbfc3928980cada60abe717ff
natemoo-re <nate.moore.27@gmail.com>
2021-08-04 13:04:00 -0500

    1 file modified

--- astro/plugin/wasm/init.js
+++ astro/plugin/wasm/init.js
@@ -1,11 +1,12 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import './exec.cjs';
 
 async function init() {
   const go = new Go();
   const src = fileURLToPath(new URL('./tycho.wasm', import.meta.url));
-  const buf = fs.readFileSync(src);
+  const buf = readFileSync(src);
+  console.log(buf);
   const result = await WebAssembly.instantiate(
     new Uint8Array(buf),
     go.importObject
